15e25ac4c3296aa76fb2dde5c5e80a4911563177
J Bruce Morton <JBM@Anabels-MacBook-Pro-2.local>
2018-03-30 12:25:42 -0400

    version change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='geoweather',
-      version='0.25',
+      version='0.26',
       description='Useful weather functions',
       url='http://github.com/nanodan/geoweather',
       author='Daniel J. Lewis',
